df7f8d549cfdf90b7f8021ebc8f5265ab81f84ed
Pedro <barbeitos.pedro@gmail.com>
2023-12-28 00:07:35 +0000

    First commit

---
+++ components/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <div className="flex flex-wrap justify w-full">
+      <div>pedro barbeitos</div>
+      <div>
+        <div>about</div>
+        <div>work</div>
+      </div>
+    </div>
+  );
+}
