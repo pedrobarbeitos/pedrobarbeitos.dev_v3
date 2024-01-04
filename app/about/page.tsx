d440f6eed3a60f40a0e30f5f857bd7024a864ab6
Pedro <barbeitos.pedro@gmail.com>
2024-01-04 21:38:40 +0000

    about page started

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,14 +1,9 @@
-import BackImage from "@/components/BackImage";
 import Footer from "@/components/Footer";
-import A01 from "/public/01A.jpg";
-import B01 from "/public/01B.jpg";
 
 export default function About() {
   return (
     <main className=" grow flex w-full flex-col items-center">
-      <section className="flex grow w-full">
-        <BackImage imageOne={A01} imageTwo={B01} />
-      </section>
+      <section className="flex grow w-full">my about page</section>
       <Footer />
     </main>
   );
