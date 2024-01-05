1f98799cb1e7a73c33465d728b7f6e5532a7b413
Pedro <barbeitos.pedro@gmail.com>
2024-01-05 23:07:16 +0000

    aphostrophe linting corrected

--- app/terms/page.tsx
+++ app/terms/page.tsx
@@ -17,14 +17,14 @@ export default function Terms() {
           applies to these Terms and Conditions, Privacy Statement and
           Disclaimer Notice and all Agreements: "Client", "You" and "Your"
           refers to you, the person log on this website and compliant to the
-          Company’s terms and conditions. "The Company", "Ourselves", "We",
+          Company&aposs terms and conditions. "The Company", "Ourselves", "We",
           "Our" and "Us", refers to our Company. "Party", "Parties", or "Us",
           refers to both the Client and ourselves. All terms refer to the offer,
           acceptance and consideration of payment necessary to undertake the
           process of our assistance to the Client in the most appropriate manner
-          for the express purpose of meeting the Client’s needs in respect of
-          provision of the Company’s stated services, in accordance with and
-          subject to, prevailing law of Netherlands. Any use of the above
+          for the express purpose of meeting the Client&aposs needs in respect
+          of provision of the Company&aposs stated services, in accordance with
+          and subject to, prevailing law of Netherlands. Any use of the above
           terminology or other words in the singular, plural, capitalization
           and/or he/she or they, are taken as interchangeable and therefore as
           referring to same.
@@ -35,8 +35,8 @@ export default function Terms() {
         <p className="leading-7 text-sm [&:not(:first-child)]:mt-6 text-justify">
           We employ the use of cookies. By accessing Pedro Barbeitos, you agreed
           to use cookies in agreement with the Alex Rabin's Privacy Policy. Most
-          interactive websites use cookies to let us retrieve the user’s details
-          for each visit. Cookies are used by our website to enable the
+          interactive websites use cookies to let us retrieve the user&aposs
+          details for each visit. Cookies are used by our website to enable the
           functionality of certain areas to make it easier for people visiting
           our website. Some of our affiliate/advertising partners may also use
           cookies.
