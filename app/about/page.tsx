8a7a76ab2333dd196723ea65fbbd936eb00e79fe
Pedro <barbeitos.pedro@gmail.com>
2024-01-05 23:21:00 +0000

    &apos added

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -14,19 +14,19 @@ export default function About() {
           conditions. Do not continue to use Alex Rabin if you do not agree to
           take all of the terms and conditions stated on this page. The
           following terminology applies to these Terms and Conditions, Privacy
-          Statement and Disclaimer Notice and all Agreements: "Client", "You"
-          and "Your" refers to you, the person log on this website and compliant
-          to the Company&aposs terms and conditions. "The Company", "Ourselves",
-          "We", "Our" and "Us", refers to our Company. "Party", "Parties", or
-          "Us", refers to both the Client and ourselves. All terms refer to the
-          offer, acceptance and consideration of payment necessary to undertake
-          the process of our assistance to the Client in the most appropriate
-          manner for the express purpose of meeting the Client&aposs needs in
-          respect of provision of the Company&aposs stated services, in
-          accordance with and subject to, prevailing law of Netherlands. Any use
-          of the above terminology or other words in the singular, plural,
-          capitalization and/or he/she or they, are taken as interchangeable and
-          therefore as referring to same.
+          Statement and Disclaimer Notice and all Agreements: Client, You and
+          Your refers to you, the person log on this website and compliant to
+          the Company&aposs terms and conditions. The Company, Ourselves, We,
+          Our and Us , refers to our Company. Party , Parties , or Us , refers
+          to both the Client and ourselves. All terms refer to the offer,
+          acceptance and consideration of payment necessary to undertake the
+          process of our assistance to the Client in the most appropriate manner
+          for the express purpose of meeting the Client&aposs needs in respect
+          of provision of the Company&aposs stated services, in accordance with
+          and subject to, prevailing law of Netherlands. Any use of the above
+          terminology or other words in the singular, plural, capitalization
+          and/or he/she or they, are taken as interchangeable and therefore as
+          referring to same.
         </p>
         <h4 className="scroll-m-20 text-2xl font-semibold tracking-tight mt-6">
           Cookies
